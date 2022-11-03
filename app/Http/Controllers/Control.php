57bde7b95904a6b0a38031877d81451cbef99cb0
PietroDanieli <1pie7d@gmail.com>
2022-11-03 19:35:10 +0100

    change fold

--- app/Http/Controllers/Control.php
+++ app/Http/Controllers/Control.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace App\Http\Controllers\Controllers;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class Control extends Controller
